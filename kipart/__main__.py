f6008b6defc6d9ce9984020b87ea942ab13be692
Dave Vandenbout <devb@xess.com>
2019-09-17 20:05:01 -0400

    Kipart now creates individual .lib files when given multiple .csv files and no global output .lib file is specified using the -o option.

--- kipart/__main__.py
+++ kipart/__main__.py
@@ -25,6 +25,7 @@ from __future__ import absolute_import, print_function
 import argparse as ap
 import io
 import os
+import re
 import sys
 import zipfile
 from collections import OrderedDict
@@ -132,37 +133,36 @@ def main():
 
     args = parser.parse_args()
 
-    if args.output == None:
-        issue("No output file specified. Using kipart.lib as the default.")
-        args.output = "kipart.lib"
-    else:
-        args.output = os.path.splitext(args.output)[0] + ".lib"
+    # kipart f1.csv f2.csv              # Create f1.lib, f2.lib
+    # kipart f1.csv f2.csv -w           # Overwrite f1.lib, f2.lib
+    # kipart f1.csv f2.csv -a           # Append to f1.lib, f2.lib
+    # kipart f1.csv f2.csv -o f.lib     # Create f.lib
+    # kipart f1.csv f2.csv -w -o f.lib  # Overwrite f.lib
+    # kipart f1.csv f2.csv -a -o f.lib  # Append to f.lib
 
-    import re
+    def read_lib_file(lib_file):
+        parts_lib = OrderedDict()
+        with open(lib_file, "r") as lib:
+            part_def = ""
+            for line in lib:
+                start = re.match("DEF (?P<part_name>\S+)", line)
+                end = re.match("ENDDEF$", line)
+                if start:
+                    part_def = line
+                    part_name = start.group("part_name")
+                elif end:
+                    part_def += line
+                    parts_lib[part_name] = part_def
+                else:
+                    part_def += line
+        return parts_lib
 
-    parts_lib = OrderedDict()
-    if os.path.isfile(args.output):
-        if not args.overwrite and not args.append:
-            print(
-                "Output file {} already exists! Use the --overwrite option to replace it or the --append option to append to it.".format(
-                    args.output
-                )
-            )
-            sys.exit(1)
-        if args.append:
-            with open(args.output, "r") as lib:
-                part_def = ""
-                for line in lib:
-                    start = re.match("DEF (?P<part_name>\S+)", line)
-                    end = re.match("ENDDEF$", line)
-                    if start:
-                        part_def = line
-                        part_name = start.group("part_name")
-                    elif end:
-                        part_def += line
-                        parts_lib[part_name] = part_def
-                    else:
-                        part_def += line
+    def write_lib_file(parts_lib, lib_file):
+        LIB_HEADER = "EESchema-LIBRARY Version 2.3\n"
+        with open(lib_file, "w") as lib_fp:
+            lib_fp.write(LIB_HEADER)
+            for part_def in parts_lib.values():
+                lib_fp.write(part_def)
 
     def call_kipart(part_data_file):
         """Helper routine for calling kipart."""
@@ -180,27 +180,65 @@ def main():
 
     DEFAULT_PIN.side = args.side
 
+    check_file_exists = True  # Used to check for existence of a single output lib file.
+
     for input_file in args.input_files:
-        file_ext = os.path.splitext(input_file)[-1]
+
+        # No explicit output lib file, so each individual input file will generate its own .lib file.
+        if check_file_exists or not args.output:
+            output_file = args.output or os.path.splitext(input_file)[0] + ".lib"
+            if os.path.isfile(output_file):
+                # The output lib file already exists.
+                if args.overwrite:
+                    # Overwriting an existing file, so ignore the existing parts.
+                    parts_lib = OrderedDict()
+                elif args.append:
+                    # Appending to an existing file, so read in existing parts.
+                    parts_lib = read_lib_file(output_file)
+                else:
+                    print(
+                        "Output file {} already exists! Use the --overwrite option to replace it or the --append option to append to it.".format(
+                            output_file
+                        )
+                    )
+                    sys.exit(1)
+            else:
+                # Lib file doesn't exist, so create a new lib file starting with no parts.
+                parts_lib = OrderedDict()
+
+        # Don't setup the output lib file again if -o option was used to specify a single output lib.
+        check_file_exists = not args.output
+
+        file_ext = os.path.splitext(input_file)[-1]  # Get input file extension.
+
         if file_ext == ".zip":
+            # Process the individual files inside a ZIP archive.
             zip_file = zipfile.ZipFile(input_file, "r")
             zipped_files = zip_file.infolist()
             for zipped_file in zipped_files:
                 if os.path.splitext(zipped_file.filename)[-1] in [".csv", ".txt"]:
+                    # Only process CSV and TXT files in the archive.
                     with zip_file.open(zipped_file, "r") as part_data_file:
                         part_data_file = io.TextIOWrapper(part_data_file)
                         call_kipart(part_data_file)
+
         elif file_ext in [".csv", ".txt"]:
+            # Process CSV and TXT files.
             with open(input_file, "r") as part_data_file:
                 call_kipart(part_data_file)
+
         else:
+            # Skip unrecognized files.
             continue
 
-    LIB_HEADER = "EESchema-LIBRARY Version 2.3\n"
-    with open(args.output, "w") as lib_file:
-        lib_file.write(LIB_HEADER)
-        for part_num, part_defn in parts_lib.items():
-            lib_file.write(part_defn)
+        if not args.output:
+            # No global output lib file, so output a lib file for each input file.
+            write_lib_file(parts_lib, output_file)
+
+    if args.output:
+        # Only a single lib output file was given, so write library to it after all
+        # the input files were processed.
+        write_lib_file(parts_lib, output_file)
 
 
 # main entrypoint.
