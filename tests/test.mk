54d9153913ef5bc8db4a0054fc891285301a81c8
Dave Vandenbout <devb@xess.com>
2019-09-18 07:23:00 -0400

    Added more random tests of kipart and kilib2csv.

--- tests/test.mk
+++ tests/test.mk
@@ -2,17 +2,41 @@ tests := example1 example2 example3 example4 example5 example6 lt1512 helwig tes
 examples := example1 example2 example3 example4 example5 example6
 
 #all: randomtest
-all: randomtest $(tests:=.tst)
-clean: $(tests:=.clean)
+all: randomtest1 randomtest2 randomtest3 $(tests:=.tst)
+clean: randomtest_clean $(tests:=.clean)
 examples: $(examples:=.lib)
 tests: $(tests:=.tst)
 
-randomtest:
-	python random_csv.py > randomtest.csv
-	kipart randomtest.csv -o randomtest.lib -w
-	kilib2csv randomtest.lib -o randomtest2.csv -w
-	@/bin/diff -s randomtest.csv randomtest2.csv
+randomtest1:
+	@python random_csv.py > randomtest.csv
+	@kipart randomtest.csv -o randomtest.lib -w
+	@kilib2csv randomtest.lib -o randomtest2.csv -w
+	@-/bin/diff -s randomtest.csv randomtest2.csv
 	@echo "*********************************************************************"
+
+randomtest2:
+	@python random_csv.py > randomtest1.csv
+	@python random_csv.py > randomtest2.csv
+	@kipart randomtest1.csv randomtest2.csv -o randomtest.lib -w
+	@-/bin/sort -u randomtest.lib > randomtest_sorted.lib
+	@kipart randomtest1.csv randomtest2.csv -w
+	@cat randomtest1.lib randomtest2.lib > randomtest3.lib
+	@-/bin/sort -u randomtest3.lib > randomtest3_sorted.lib
+	@-/bin/diff -s randomtest_sorted.lib randomtest3_sorted.lib
+	@echo "*********************************************************************"
+
+randomtest3:
+	@python random_csv.py > randomtest1.csv
+	@python random_csv.py > randomtest2.csv
+	@kipart randomtest1.csv randomtest2.csv -w
+	@kilib2csv randomtest1.lib -o randomtest1_rebuilt.csv -w
+	@kilib2csv randomtest2.lib -o randomtest2_rebuilt.csv -w
+	@-/bin/diff -s randomtest1.csv randomtest1_rebuilt.csv
+	@-/bin/diff -s randomtest2.csv randomtest2_rebuilt.csv
+	@echo "*********************************************************************"
+
+randomtest_clean:
+	@rm -f randomtest*.csv randomtest*.lib
 
 example1.lib: example1.csv
 	kipart $^ -o $@ -w
