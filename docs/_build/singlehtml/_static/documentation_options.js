cf555612405a9df87da09d9c38a5680b548d554d
xesscorp <devb@xess.com>
2020-05-25 14:52:40 -0400

    Rebuilt documentation.

--- docs/_build/singlehtml/_static/documentation_options.js
+++ docs/_build/singlehtml/_static/documentation_options.js
@@ -1,11 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.42',
+    VERSION: '0.1.43',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'singlehtml',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
 };
