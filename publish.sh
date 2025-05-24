41cabd9d2754e1aa4d92cff4e5a328fdbea3071c
devbisme <devb@xess.com>
2025-05-24 12:13:54 -0400

    Update script for publishing kipart to PyPi and github.

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rm -f dist/*.gz
-python setup.py sdist
+rm -f dist/*
+python -m build
 python -m twine check dist/*
 python -m twine upload --repository pypi dist/*
 git tag -f `kipart -v | cut -d' ' -f2`
