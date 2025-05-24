506cebc4346d121c03df5113a130e8c8182f7475
devbisme <devb@xess.com>
2025-05-24 12:10:40 -0400

    - Corrected example CSV files to remove errors when processed by kipart. - test.mk now regenerates all the example symbol library files.

---
+++ tests/examples/test.mk
@@ -0,0 +1,20 @@
+tests := example1 example2 example3 example4 helwig hidden_test hyozd lt1512 multi_sides pin_length spaces_test test1
+
+all: $(tests:=.tst)
+
+clean: $(tests:=.clean)
+
+# This prevents .kicad_Sym files from being removed after they're made.
+.SECONDARY:
+
+FLAGS= -s num
+
+%.kicad_sym: %.csv
+	kipart $(FLAGS) $^ -o $@ -w
+
+%.tst : %.kicad_sym
+	@echo "*********************************************************************"
+
+%.clean : %.kicad_sym
+	@echo "Cleaning up $^"
+	@rm -f $^
