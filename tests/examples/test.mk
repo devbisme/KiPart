44135c1361c6dd2a1a8a9ffdc9e18104be49e0c9
devbisme <devb@xess.com>
2026-03-17 13:03:55 -0400

    SPD test files combined into a single file.

--- tests/examples/test.mk
+++ tests/examples/test.mk
@@ -1,16 +1,16 @@
-tests := example1 example2 example3 example4 helwig hidden_test hyozd lt1512 multi_sides pin_length property_test spaces_test test1 74hc_logic dram dual_opamp mixed_types opa2277 rt9818
+tests := example1 example2 example3 example4 helwig hidden_test hyozd lt1512 multi_sides pin_length property_test spaces_test test1 grabbag
 
 all: $(tests:=.tst)
 
 clean: $(tests:=.clean)
 
-# This prevents .kicad_Sym files from being removed after they're made.
+# This prevents .kicad_sym files from being removed after they're made.
 .SECONDARY:
 
 FLAGS= -b
 
-%.kicad_sym: %.sdt
-	sdt2csv $^ | kipart -w -o $@
+%.kicad_sym: %.spd
+	spd2csv $^ | kipart -w -o $@
 
 %.kicad_sym: %.csv
 	kipart $(FLAGS) $^ -o $@ -w
